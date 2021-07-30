ab65bac52f026032ac7a4cd18f4f0c1474075055
Tarntanate Memanee <tarntanate.drivemate@gmail.com>
2021-07-30 17:01:17 +0700

    Initial project

---
+++ src/utils/Networks.js
@@ -0,0 +1,9 @@
+
+export const Networks = {
+    MainNet: 1,
+    Rinkeby: 4,
+    Ropsten: 3,
+    Kovan: 42,
+    BSCMainNet: 56,
+    BSCTestNet: 97, // BSC Testnet(ChainID 0x61, 97 in decimal)
+};
---
+++ src/utils/index.js
@@ -0,0 +1,39 @@
+// import { Web3Provider } from "@ethersproject/providers";
+// import { Key } from "swr"
+// import { ABI } from "../abi/index";
+import { Contract } from "@ethersproject/contracts";
+import memoize from "fast-memoize";
+
+export function buildContract(address, abi, library) {
+  return new Contract(address, abi, library.getSigner());
+}
+
+export const memoizedBuildContract = memoize(buildContract);
+
+export const ethMethodKey = (args) => {
+  return args;
+};
+
+export const ethContractKey = (args) => {
+  return [...args];
+};
+
+export const web3Fetcher =
+  (library, ABIs) =>
+  async (...args) => {
+    // debugger
+    const [abi] = args;
+    const isContract = ABIs.get(abi) !== undefined;
+    if (isContract) {
+      const [_, address, method, ...params] = args;
+      // extract as buildContract memoized
+      const contract = buildContract(address, ABIs.get(abi), library);
+      console.log({ contract: address, method, params });
+      return contract[method](...params);
+    }
+    const [method, ...params] = args;
+    console.log({ method, params });
+    return library[method](...params);
+  };
+
+
